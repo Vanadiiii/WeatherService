6866a54155a0074ffb6ab7355eef582850f56107
IvanMatveev <vanadiiii42@gmail.com>
2021-01-05 21:18:12 +0400

    add docker-compose.yaml

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #create *.jar
 mvn clean package
 #create docker image
-docker build -t vanadiiii/weather:1.1 .
-#run image
-docker run --name weather -p 8080:8080 -d vanadiiii/weather:1.1
+docker-compose up -d
